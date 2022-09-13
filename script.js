df79c8e553bf80ab973fdc4e33d797bdd7190c34
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-13 16:52:27 -0400

    simplify code, new win conditions

---
+++ script.js
@@ -0,0 +1,18 @@
+(function () {
+    console.log(`Hello World!`);
+    
+    //global variables
+    let oTurn;
+    const X_class = "x";
+    const O_class = "o";
+    const winningCombination = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
